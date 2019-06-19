6b2b6e4ad357eaa993328b244043e4b0ce55e8b5
Grzegorz Kastelik <g.kastelik@gmail.com>
2019-06-19 02:04:02 +0200

    add todo

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('/login', 'AuthController@loginUser');
     Route::post('/register', 'AuthController@registerUser');
-    Route::post('/authorize', 'AuthController@authorizeLogedUser'); //TODO add test
+    // TODO remove role from there - only client
+    // TODO add password validation for register user
+    // TODO add test for password validation
+
+    Route::post('/authorize', 'AuthController@authorizeLogedUser');
+    // TODO add test
+    
+    // TODO add reset password endpoint
 });
