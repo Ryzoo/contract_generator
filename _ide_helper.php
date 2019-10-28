206d04ce13d663701a3da148e6ce93803f7f49d0
Grzegorz Kastelik <g.kastelik@gmail.com>
2019-10-28 18:16:43 +0100

    update composer dep

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.0.0 on 2019-10-27 21:28:34.
+ * Generated for Laravel 6.0.0 on 2019-10-28 17:09:51.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
