3dad28c07986a0da8766a13412b547f834c0b92b
Grzegorz Kastelik <g.kastelik@gmail.com>
2019-10-28 23:24:46 +0100

    updater

--- recipe/copy_recipe.php
+++ recipe/copy_recipe.php
@@ -2,4 +2,4 @@
 
 namespace Deployer;
 
-task('copy', "cp -LRf ../../current ../../public_html/actual 2>/dev/null");
+task('copy', "rm -rf ../../public_html/actual && cp -LRf ../../current ../../public_html/actual 2>/dev/null");
