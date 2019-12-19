94f37b1ecd7916afe7ce34d63f31cd4877888521
JeremyCoco <tonyawake4@gmail.com>
2019-12-19 10:15:51 +0100

    add styles to suggestions

--- resources/js/components/Contract/Builder/Blocks/Types/TextBlock.vue
+++ resources/js/components/Contract/Builder/Blocks/Types/TextBlock.vue
@@ -318,4 +318,68 @@
             }
         }
     }
+
+    .mention {
+      background: rgba(#2a272f, 0.1);
+      color: rgba(#2a272f, 0.6);
+      font-size: 0.8rem;
+      font-weight: bold;
+      border-radius: 5px;
+      padding: 0.2rem 0.5rem;
+      white-space: nowrap;
+    }
+    .mention-suggestion {
+      color: rgba(#2a272f, 0.6);
+    }
+    .suggestion-list {
+      padding: 0.2rem;
+      border: 2px solid rgba(#2a272f, 0.1);
+      font-size: 0.8rem;
+      font-weight: bold;
+      &__no-results {
+        padding: 0.2rem 0.5rem;
+      }
+      &__item {
+        border-radius: 5px;
+        padding: 0.2rem 0.5rem;
+        margin-bottom: 0.2rem;
+        cursor: pointer;
+        &:last-child {
+          margin-bottom: 0;
+        }
+        &.is-selected,
+        &:hover {
+          background-color: rgba(#fff, 0.2);
+        }
+        &.is-empty {
+          opacity: 0.5;
+        }
+      }
+    }
+    .tippy-tooltip.dark-theme {
+      background-color: #2a272f;
+      padding: 0;
+      font-size: 1rem;
+      text-align: inherit;
+      color: #fff;
+      border-radius: 5px;
+      .tippy-backdrop {
+        display: none;
+      }
+      .tippy-roundarrow {
+        fill: #2a272f;
+      }
+      .tippy-popper[x-placement^=top] & .tippy-arrow {
+        border-top-color: #2a272f;
+      }
+      .tippy-popper[x-placement^=bottom] & .tippy-arrow {
+        border-bottom-color: #2a272f;
+      }
+      .tippy-popper[x-placement^=left] & .tippy-arrow {
+        border-left-color: #2a272f;
+      }
+      .tippy-popper[x-placement^=right] & .tippy-arrow {
+        border-right-color: #2a272f;
+      }
+    }
 </style>
