fd1fe7b69bad5b4df37a32af65251ffba560eebf
Grzegorz Kastelik <g.kastelik@gmail.com>
2020-01-15 06:47:42 +0100

    fix symlink

--- deployer/common_recipe.php
+++ deployer/common_recipe.php
@@ -3,4 +3,4 @@
 namespace Deployer;
 
 
-task('symlink', "rm -rf ../../public_html/ && ln -s ../../current/public ../../public_html");
+task('symlink', "rm -rf ../../public_html && ln -s ~/domains/generatorumowy.pl/current/public ~/domains/generatorumowy.pl/public_html");
