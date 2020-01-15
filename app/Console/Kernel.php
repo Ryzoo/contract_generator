767c07780595fc9749165ed85a72a884595e7927
Grzegorz Kastelik <g.kastelik@gmail.com>
2020-01-15 07:11:51 +0100

    fix symlink

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,41 +7,41 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        Commands\ProcessContractRender::class,
-    ];
+  /**
+   * The Artisan commands provided by your application.
+   *
+   * @var array
+   */
+  protected $commands = [
+    Commands\ProcessContractRender::class,
+  ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        $schedule->command('contract:render')
-            ->everyMinute()
-            ->withoutOverlapping()
-            ->after(function () use($schedule) {
-                $schedule->command('queue:work --stop-when-empty')
-                    ->withoutOverlapping();
-            });
-    }
+  /**
+   * Define the application's command schedule.
+   *
+   * @param Schedule $schedule
+   * @return void
+   */
+  protected function schedule(Schedule $schedule)
+  {
+    $schedule->command('contract:render')
+      ->everyMinute()
+      ->withoutOverlapping();
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+    $schedule->command('queue:work --stop-when-empty')
+      ->everyMinute()
+      ->withoutOverlapping();
+  }
 
-        require base_path('routes/console.php');
-    }
+  /**
+   * Register the commands for the application.
+   *
+   * @return void
+   */
+  protected function commands()
+  {
+    $this->load(__DIR__ . '/Commands');
+
+    require base_path('routes/console.php');
+  }
 }
