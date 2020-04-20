fcd2c494c5cf4867459e3b6af2c9c8c925f01cad
Marek Bubiak <marek.bubiak@euvic.pl>
2020-04-20 18:54:46 +0200

    change page divider v1

--- resources/js/components/Contract/Builder/Blocks/Types/PageDivideBlock.vue
+++ resources/js/components/Contract/Builder/Blocks/Types/PageDivideBlock.vue
@@ -4,6 +4,25 @@
         <v-btn @click="removeBlock" class="mx-3" text icon>
             <v-icon>fa-trash</v-icon>
         </v-btn>
+      <v-btn @click="editBlock" class="mx-3" text icon>
+        <v-icon>fa-edit</v-icon>
+      </v-btn>
+      <v-dialog
+        v-model="divideBlockModal"
+        scrollable
+        max-width="800px">
+        <v-card>
+          <v-card-title>Block configuration:<small class="pl-3" style="color: #7a7a7a">{{block.blockName}}</small></v-card-title>
+          <v-card-text>
+            elo
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer/>
+            <v-btn color="primary" outlined @click="pushCloseEvent">Cancel</v-btn>
+            <v-btn color="primary" @click="saveBlockConfiguration">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </section>
 </template>
 
@@ -11,10 +30,18 @@
 export default {
   name: 'PageDivideBlock',
   props: ['block'],
+  data () {
+    return {
+      divideBlockModal: false
+    }
+  },
   methods: {
     removeBlock () {
       const newBlocks = this.removeFromData(this.$store.getters.builder_allBlocks, this.block.id)
       this.$store.dispatch('builder_set', newBlocks)
+    },
+    editBlock () {
+      this.divideBlockModal = true
     },
     removeFromData (dataArray, idToRemove) {
       if (dataArray.find(x => x.id === idToRemove)) {
