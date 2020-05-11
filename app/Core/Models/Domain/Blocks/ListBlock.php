fab49021206d7deaa20640b53d3484712f5dfa65
Grzegorz Kastelik <g.kastelik@gmail.com>
2020-05-11 18:09:22 +0200

    refactor block class

--- app/Core/Models/Domain/Blocks/ListBlock.php
+++ app/Core/Models/Domain/Blocks/ListBlock.php
@@ -12,9 +12,10 @@ use Illuminate\Support\Facades\Validator;
 use App\Core\Models\Database\Contract;
 use Illuminate\Support\Collection;
 
-class ListBlock extends Block {
+class ListBlock extends EmptyBlock {
 
   public function __construct() {
+    parent::__construct();
     $this->initialize(BlockType::LIST_BLOCK);
   }
 
@@ -24,72 +25,9 @@ class ListBlock extends Block {
     ];
   }
 
-  protected function validateContent(): bool {
-    Validator::validate($this->content, [
-      'blocks' => 'nullable|array',
-    ]);
-
-    return TRUE;
-  }
-
-  protected function buildContent(): void {
-    $this->content['blocks'] = Block::getListFromString(json_encode($this->content['blocks'], JSON_THROW_ON_ERROR, 512));
-  }
-
-  protected function resolveAttributesInContent(Collection $formElements, Attribute $repeatAttribute = NULL, $repeatValue = NULL): void {
-    $blockList = $this->content['blocks'];
-
-    /** @var \App\Core\Models\Domain\Blocks\Block $block */
-    foreach ($blockList as $block) {
-      $block->resolveAttributesInContent($formElements, $repeatAttribute, $repeatValue);
-    }
-  }
-
-  public function findVariable(Contract $contract): Collection {
-    $variableArray = parent::findVariable($contract);
-
-    /** @var \App\Core\Models\Domain\Blocks\Block $block */
-    foreach ($this->content['blocks'] as $block) {
-      $variableArray = $variableArray->merge($block->findVariable($contract));
-    }
-
-    return $variableArray;
-  }
-
-  public function counterResolve(string $matchString, int $countStart, Contract $contract): int {
-    $countStart = parent::counterResolve($matchString, $countStart, $contract);
-    $countStart = BlockCounterResolver::resolve($matchString, collect($this->content['blocks']), $contract, $countStart)['count'];
-    return $countStart;
-  }
-
-  public function getBlockCollection(Collection $blockCollection): Collection {
-    $blockCollection = parent::getBlockCollection($blockCollection);
-
-    /** @var \App\Core\Models\Domain\Blocks\Block $block */
-    foreach ($this->content['blocks'] as $block) {
-      $blockCollection = $block->getBlockCollection($blockCollection);
-    }
-
-    return $blockCollection;
-  }
-
-  public function validateConditions(int $conditionalType, Collection $formElements, Contract $contract, int $index = 0): bool {
-    $parentActive = parent::validateConditions($conditionalType, $formElements, $contract, $index);
-
-    if ($parentActive) {
-      /** @var \App\Core\Models\Domain\Blocks\Block $block */
-      foreach ($this->content['blocks'] as $block) {
-        $block->validateConditions($conditionalType, $formElements, $contract, $index);
-      }
-
-      $this->content['blocks'] = collect($this->content['blocks'])->where('isActive');
-    }
-
-    return $parentActive;
-  }
-
   public function renderToHtml(Collection $attributes, Attribute $repeatAttribute = NULL, $repeatValue = NULL): string {
-    $htmlString = parent::renderToHtml($attributes, $repeatAttribute, $repeatValue);
+    $this->resolveAttributesInContent($attributes, $repeatValue, $repeatValue);
+    $htmlString = '';
     $blockList = $this->content['blocks'];
     $htmlString .= $this->getListStartTag();
 
--- app/Core/Models/Domain/Blocks/ListItemBlock.php
+++ app/Core/Models/Domain/Blocks/ListItemBlock.php
@@ -12,66 +12,9 @@ use Illuminate\Support\Facades\Validator;
 use App\Core\Models\Database\Contract;
 use Illuminate\Support\Collection;
 
-class ListItemBlock extends Block {
-
+class ListItemBlock extends TextBlock {
   public function __construct() {
+    parent::__construct();
     $this->initialize(BlockType::LIST_ITEM_BLOCK);
-  }
-
-  protected function buildSettings():void {}
-
-  protected function buildContent():void {
-    if(!isset($this->content['text'])) {
-      $this->content = [
-        'text' => ''
-      ];
-    }
-  }
-
-  protected function validateContent(): bool {
-    Validator::validate($this->content, [
-      'text' => 'nullable|string',
-    ]);
-
-    $this->content['text'] = $this->content['text'] ?? '';
-
-    return TRUE;
-  }
-
-  protected function resolveAttributesInContent(Collection $formElements, Attribute $repeatAttribute = null, $repeatValue = null):void {
-    $attributeResolver = new AttributeResolver($formElements);
-    $this->content['text'] = $attributeResolver->resolveText($this->content['text'], true);
-    if(isset($repeatAttribute)){
-      $this->content['text'] = MultiAttributeResolver::resolve($repeatAttribute, $this->content['text'], $repeatValue);
-    }
-  }
-
-  public function counterResolve(string $matchString, int $countStart, Contract $contract): int {
-    $attributeResolver = new CounterResolver($matchString, $countStart, $contract);
-    $this->content['text'] = $attributeResolver->resolveText($this->content['text']);
-    return $attributeResolver->getCounter();
-  }
-
-  public function findVariable(Contract $contract): Collection {
-    $variableArray = parent::findVariable($contract);
-
-    preg_match_all('/{(\d+)}/', $this->content['text'], $output_array);
-
-    foreach ($output_array[1] as $output) {
-      $variableArray->push([$this->id, $output]);
-    }
-
-    preg_match_all('/{(\d+):\d+}/', $this->content['text'], $output_array);
-
-    foreach ($output_array[1] as $output) {
-      $variableArray->push([$this->id, $output]);
-    }
-
-    return $variableArray;
-  }
-
-  public function renderToHtml(Collection $attributes, Attribute $repeatAttribute = null, $repeatValue = null): string {
-    $htmlString = parent::renderToHtml($attributes, $repeatAttribute, $repeatValue);
-    return $htmlString . $this->content['text'];
   }
 }
